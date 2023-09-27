ac4b96bc1dffc3deee7a877352183e4026fa710c
Stuart Bate <bate.stuart93@gmail.com>
2023-09-27 16:05:05 -0700

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Doug",
+    streetAddress: "3221 Simon St"
+}
+
+function updateEmployeeWithKeyAndValue() {
+    const eeUpdate = { ...employee }
+
+    eeUpdate["name"] = "Sam";
+    eeUpdate["streetAddress"] = "11 Broadway";
+    
+    return eeUpdate;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const eeFired = { ...employee };
+
+    delete eeFired["name"];
+    return eeFired;
+};
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+};
